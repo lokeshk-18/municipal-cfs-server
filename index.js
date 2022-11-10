3bb5a8a7c3dc95af44aa348ac01dd350bfff7e3c
Lokesh K <lokeshkl12318@gmail.com>
2022-11-10 23:43:11 +0530

    vom

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express= require("express")
-// const cors=require("cors")
+const cors=require("cors")
 const mongoose=require('mongoose');
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy; 
@@ -26,10 +26,12 @@ db.once("open",()=>{
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-// app.use(cors(
-
-// ))
-
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}))
 
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended:true}));
